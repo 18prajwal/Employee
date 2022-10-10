638db27f8193ad4c11e403a47f775cfc42df9c86
amantya <bhopaleprajwal00@gmail.com>
2022-10-10 17:59:26 +0530

    Synchronized files

---
+++ WithoutSynchronisationTwoThreads.java
@@ -0,0 +1,52 @@
+ class DemoSync{
+    int count=0;
+       void incre(){
+          System.out.println("not synch"+Thread.currentThread().getName());
+
+              count++;
+
+    }
+}
+public class WithoutSynchronisationTwoThreads{
+     public static void main(String[] args) throws Exception {
+         DemoSync mo = new DemoSync();
+         class T1 extends Thread {
+
+             public void run(){
+
+                 for(int i=0;i<1000;i++){
+                     mo.incre();
+                 }
+                 //System.out.println(mo.count);
+             }
+
+         }
+
+         class T2 extends Thread{
+
+            // DemoSync mo=new DemoSync();
+             public void run(){
+                 for(int i=0;i<1000;i++){
+                     mo.incre();
+                 }
+
+
+             }
+
+         }
+         T1 obj=new T1();
+         T2 obj1=new T2();
+         obj.start();
+         obj1.start();
+         obj1.join();
+         
+
+         System.out.println(mo.count);
+
+
+     }
+}
+
+
+
+
